6c09b865a9a1cd1dcaa8446b15d9a7c24b449d3d
Caleb Xavier Berger <caleb.x.berger@gmail.com>
2020-01-07 04:05:10 -0500

    Install qemu binaries

--- ci/build.sh
+++ ci/build.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
 if [ "${CARGO}" == "cross" ]; then
+    sudo apt-get install -y qemu-user-static
     # set up docker
-    cargo install cross
+    echo Installing cross
+    cargo install --force cross
     sudo systemctl start docker
     echo Building docker image
     $(pwd)/cross-build.sh
