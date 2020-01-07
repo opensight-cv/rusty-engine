298a6ea251af8920863b0b3350e8ed6495c6cca9
Caleb Xavier Berger <caleb.x.berger@gmail.com>
2020-01-07 03:11:45 -0500

    Move docker starting and cross installation to build.sh

--- ci/build.sh
+++ ci/build.sh
@@ -1,5 +1,7 @@
-if [ "$1" == "cross" ]; then
+if [ "${CARGO}" == "cross" ]; then
     # set up docker
+    cargo install cross
+    sudo systemctl start docker
     echo Building docker image
     $(pwd)/cross-build.sh
 fi
