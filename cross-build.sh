1d87d0b239294873d91f69d2b146cdffba495b8e
Caleb Xavier Berger <caleb.x.berger@gmail.com>
2020-01-07 01:24:48 -0500

    Add script to set up docker and optionally test a command

---
+++ cross-build.sh
@@ -0,0 +1,17 @@
+# bind last argument in portable manner
+for last in "$@"; do :; done
+
+docker_qemu="docker/qemu-arm-static"
+rm -f ${docker_qemu}
+cp $(which qemu-arm-static) docker/qemu-arm-static
+${DOCKER} build -t rusty-engine:latest docker/
+rm ${docker_qemu}
+
+if [ "${last}" == "run" ]; then
+    ${DOCKER} run --rm --privileged \
+        --volume "$(pwd)":/docking-bay \
+        --name "opsi-rusty-engine-arm" \
+        rusty-engine:latest \
+        bash -e -o pipefail -c \
+        "uname -a"
+fi
