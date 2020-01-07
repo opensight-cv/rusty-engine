21adfe598e475cdc02d847dad42277162725a235
Caleb Xavier Berger <caleb.x.berger@gmail.com>
2020-01-07 04:13:43 -0500

    Don't install cross if it's cached by Travis

--- ci/build.sh
+++ ci/build.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
+set -e
+
 if [ "${CARGO}" == "cross" ]; then
     sudo apt-get install -y qemu-user-static
     # set up docker
     echo Installing cross
-    cargo install --force cross
+    cargo install cross || true
     sudo systemctl start docker
     echo Building docker image
     $(pwd)/cross-build.sh
